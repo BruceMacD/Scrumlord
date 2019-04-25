8ffec1bb3360a5b49d0091bff37471ebeaa4ae08
Bruce <brucewmacdonald@gmail.com>
2019-04-24 21:03:43 -0700

    store teams

--- background.js
+++ background.js
@@ -2,7 +2,7 @@
 
 chrome.runtime.onInstalled.addListener(function() {
   // Start by loading any locally stored templates from previous sessions
-  chrome.storage.sync.get(["teams"], function(teams) {
+  chrome.storage.sync.get(["scrum-teams"], function(teams) {
     console.log('Scrumlord running...');
     console.log('Stored teams:');
     console.log(teams);
--- options_page/options.js
+++ options_page/options.js
@@ -1,62 +1,100 @@
 'use strict';
 
-var centerContainer = document.getElementById('centerContainer');
+var centerContainer = document.getElementById('center-container');
+
+function saveTeamName(oldTeamName, newTeamName) {
+  chrome.storage.sync.get(["scrum-teams"], function(storedTeams) {
+    var teams = storedTeams ? storedTeams["scrum-teams"] : {};
+    console.log(teams);
+    
+    if (oldTeamName)
+    {
+      delete teams[oldTeamName];    
+    }
+    // new empty team
+    teams[newTeamName] = [];
+
+    // update the stored templates
+    chrome.storage.sync.set({ "scrum-teams": teams }, function(){
+      console.log("teams updated");
+    });
+
+    location.reload();
+  });
+}
+
+function removeTeam(teamName) {
+  chrome.storage.sync.get(["scrum-teams"], function(storedTeams) {
+    var teams = storedTeams ? storedTeams["scrum-teams"] : {};
+    delete teams[teamName];    
+
+    chrome.storage.sync.set({ "scrum-teams": teams }, function(){
+      console.log("teams updated");
+    });
+
+    location.reload();
+  });
+}
 
 // add a new team input
-function appendTeamInput(teamNum) {
+function appendTeamInput(teamName) {
   var divGroup = document.createElement('div');
   divGroup.className = "input-group";
 
   var input = document.createElement('input');
   input.type = "text";
   input.className = "form-control";
-  input.placeholder = "Team name";
   input.setAttribute('aria-label', "Team name");
   input.setAttribute('aria-describedby', "basic-addon2");
+  if (teamName === "") {
+    input.placeholder = "Team name";    
+  } else {
+    input.value = teamName;
+  }
   divGroup.appendChild(input);
 
   var divGroupAppend = document.createElement('div');
   divGroupAppend.className = "input-group-append";
   divGroup.appendChild(divGroupAppend);
 
-  //TODO: add ids to buttons from team num for reference
   var btnSave = document.createElement('button');
   btnSave.className = "btn btn-outline-secondary";
   btnSave.type = "button";
   btnSave.innerHTML = "Save";
+  btnSave.onclick = function(element){
+    // only save if there is a set value
+    if (input.value)
+    {
+      saveTeamName(teamName, input.value);
+    }
+  }
+
   divGroupAppend.appendChild(btnSave);
 
   var btnRemove = document.createElement('button');
   btnRemove.className = "btn btn-outline-secondary";
   btnRemove.type = "button";
   btnRemove.innerHTML = "Remove";
+  btnRemove.onclick = function(element){
+    if (teamName)
+    {
+      removeTeam(teamName);
+    }
+  }
   divGroupAppend.appendChild(btnRemove);
 
   centerContainer.appendChild(divGroup);
 }
 
-// add a team input from an existing value
-function appendExistingTeamInput(teamName, teamNum) {
-  appendTeamInput(teamNum);
-  // TODO: set the value of the input area
-}
-
 // utility function for loading the stored team view
 function loadTeamView(teams) {
-  if (teams.length) {
-    // TODO: convert to Object.keys(teams).length
-    var teamCount = teams.length;
-
-    for(var i = 0; i < teamCount; i++) {
-      //TODO: appendExistingTeamInput
-    }
-    appendTeamInput(teamCount + 1);
-  } else {
-    appendTeamInput(0);
-  }
+  for(var teamName in teams) {
+    appendTeamInput(teamName);
+ }
+  appendTeamInput("");
 }
 
 // load the stored teams
-chrome.storage.sync.get(["teams"], function(teams) {
-  loadTeamView(teams);
+chrome.storage.sync.get(["scrum-teams"], function(storedTeams) {
+  loadTeamView(storedTeams["scrum-teams"]);
 });
