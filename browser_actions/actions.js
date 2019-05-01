1f3d565968f1378c7b2a8eb6a7af2cef26a3c174
Bruce <brucewmacdonald@gmail.com>
2019-04-30 20:38:26 -0700

    fix sync issue

--- browser_actions/actions.js
+++ browser_actions/actions.js
@@ -19,38 +19,13 @@ function Node(prev, teamName, personName, link, next) {
   this.next = next;
 }
 
-async function loadTeams() {
-  chrome.storage.sync.get(["scrum-teams"], function(storedTeams) {
-    var teams = storedTeams["scrum-teams"];
-    for(var team in teams) {
-      for (var member in teams[team]) {
-        var newNode = new Node(currentNode, team, member, teams[team][member], null);
-        if (currentNode === null) {
-          firstNode = newNode;
-          console.log(firstNode);
-        } else {
-          currentNode.next = newNode;
-        }
-        currentNode = newNode;
-        console.log(currentNode);
-      }
-    }
-  });
-  return 1;
-}
-
 function updateDisplay() {
   team.innerHTML = currentNode.teamName;
   member.innerHTML = currentNode.personName;
-  console.log(currentNode.link);
   chrome.tabs.update({
     // must be http://www.* format
     url: String(currentNode.link)
   });
-}
-
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 // hide to start
@@ -63,13 +38,21 @@ startBtn.onclick = async function(element) {
   title.style.display = "inline";
   start.style.display = "none";
 
-  await loadTeams();
+  chrome.storage.sync.get(["scrum-teams"], function(storedTeams) {
+    var teams = storedTeams["scrum-teams"];
+    for(var team in teams) {
+      for (var member in teams[team]) {
+        var newNode = new Node(currentNode, team, member, teams[team][member], null);
+        if (currentNode === null) {
+          firstNode = newNode;
+        } else {
+          currentNode.next = newNode;
+        }
+        currentNode = newNode;
+      }
+    }
 
-  // gotta make sure the first node get set before continuing
-  // ... an async waterfall would work too
-  while (firstNode === null) {
-    await sleep(1000);
-  }
-  currentNode = firstNode;
-  updateDisplay();
+    currentNode = firstNode;
+    updateDisplay();
+  });
 };
